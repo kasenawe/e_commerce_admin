fef6e3a825028db81d6ed60ec76a50b230ab07b9
Maximiliano Quintana <urumax@gmail.com>
2023-06-23 15:25:01 -0300

    create admin v1

--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import TableAdmins from "../components/TableAdmins";
-import Button from "react-bootstrap/Button";
+
+import CreateAdmin from "../components/CreateAdmin";
 
 function Admins() {
   return (
     <div className="text-center">
       <h1 className="mt-2">Admins</h1>
-      <Button variant="primary" className="float-end adminButton mb-2">
-        New Admin
-      </Button>
+      <CreateAdmin />
 
       <TableAdmins />
     </div>
