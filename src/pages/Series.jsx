c955120452498dcaf0db515465f8523ddc8c6b34
Bruno Manduré <brunomanduremedina99@gmail.com>
2023-06-23 15:38:39 -0300

    tableLines now lists all lines

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import TableSeries from "../components/TableSeries";
+import TableSeries from "../components/TableLines";
 import Button from "react-bootstrap/Button";
 
 function Series() {
   return (
     <div className="text-center">
-      <h1 className="mt-2">Series</h1>
+      <h1 className="mt-2">Lines</h1>
       <Button variant="primary" className="float-end adminButton mb-2">
-        Add New Serie
+        Add New Line
       </Button>
       <TableSeries />
     </div>
